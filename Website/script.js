0e5cb7dc87ff199137449ad15fc3d2e3f0735cf5
HetGameBoekje <514527@vistacollege.nl>
2022-10-12 22:47:21 +0200

    added login page

--- Website/script.js
+++ Website/script.js
@@ -31,3 +31,15 @@ var coll = document.getElementsByClassName("collapsible");
     }
   }
 }
+  // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+  function openTab(tabName) {
+    var i, x;
+    x = document.getElementsByClassName("containerTab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      
+    }
+    document.getElementById(tabName).style.display = "block";
+  
+  }
+  
